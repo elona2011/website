4818c6e5b46775567067f9e1101947fc99dd36b8
elona2011 <cdszbjj@qq.com>
2017-07-30 22:45:03 +0800

    u

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const http = require('http')
+const parse = require('url').parse
 
 http.createServer((req, res) => {
+    let reqBody = ''
+    req.on('data', d => {
+        reqBody += d
+    })
+    req.on('end', d => {
+        let url = parse(req.url)
+
+        switch (req.method) {
+            case "GET":
+                break
+            case "POST":
+                if(url.pathname === '/csrf/update'){
+                    console.log('csrf')
+                }
+                break
+        }
+    })
     res.writeHead(200, {
         "Content-Type": "text/html"
     })
@@ -11,4 +29,4 @@ http.createServer((req, res) => {
                     <body>yanjie.me网站建设中</body>
                 </html>`)
     res.end()
-}).listen(80)
+}).listen(8000)
