f4ac97851a46fd50e78a7b0aca3e0b5f5b6ab69e
elona2011 <cdszbjj@qq.com>
2017-08-09 23:15:49 +0800

    u

--- src/chart/pie/index.js
+++ src/chart/pie/index.js
@@ -13,20 +13,25 @@ class Pie {
         return this._config
     }
     set config(config) {
+        let r = 15.9155,
+            strokeWidth = 33
         this.config_default = {
             pie: {
-                cx: 20,
-                cy: 20,
+                r,
+                strokeWidth,
+                cx: r + strokeWidth / 2,
+                cy: r + strokeWidth / 2,
                 'fill-opacity': 0,
-                strokeWidth: 5
             },
             svg: {
                 width: 400,
                 height: 400,
+                viewBox: '0 0 ' + (2 * r + strokeWidth) + ' ' + (2 * r + strokeWidth)
             },
             color: ['#001f3f', '#FF851B', '#0074D9', '#FF4136', '#7FDBFF', '#85144b', '#39CCCC', '#F012BE', '#3D9970', '#B10DC9', '#FFDC00', '#DDDDDD']
         }
         Object.assign(this.config_default.pie, config && config.pie)
+        Object.assign(this.config_default.svg, config && config.svg)
         this._config = this.config_default
     }
     get data() {
@@ -39,15 +44,12 @@ class Pie {
                 num: d,
                 dashwidth: d * 100 / data.sum,
                 dashgap: 100 - d * 100 / data.sum,
-                dashoffset: 25
+                dashoffset: 25.01
             }
         })
         this._data.forEach((d, index, arr) => {
             for (let i = 0; i < index; i++) {
                 d.dashoffset -= arr[i].dashwidth
-            }
-            if (d.dashoffset < 100) {
-                d.dashoffset += 100
             }
         })
     }
@@ -55,22 +57,21 @@ class Pie {
     initDom(dom) {
         this.dom = dom
         let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+        svg.setAttribute('width',this.config.svg.width)
+        svg.setAttribute('height',this.config.svg.height)
         this.dom.appendChild(svg)
         this.svg = this.dom.querySelector('svg')
-        this.svg.setAttribute('viewBox', '0 0 40 40')
-        Object.assign(this.svg.style, this.config.svg)
+        this.svg.setAttribute('viewBox', this.config.svg.viewBox)
         this.data.forEach((d, i) => {
             this.drawPie(d, this.config.color[i])
         })
     }
 
     drawPie(d, color) {
-        let PI2 = Math.PI * 2,
-            pie = document.createElementNS('http://www.w3.org/2000/svg', 'circle'),
-            l = PI2 * this.config.pie.r
-        pie.setAttribute('r', 15.91549430918954)
-        pie.setAttribute('cx', 20)
-        pie.setAttribute('cy', 20)
+        let pie = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+        pie.setAttribute('r', this.config.pie.r)
+        pie.setAttribute('cx', this.config.pie.cx)
+        pie.setAttribute('cy', this.config.pie.cy)
         this.svg.appendChild(pie)
         pie = this.svg.querySelector('svg circle:last-child')
         Object.assign(pie.style, this.config.pie)
