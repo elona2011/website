eec7e6c534209ae167ea734c91b4e80d9a6eba63
elona2011 <cdszbjj@qq.com>
2017-09-20 07:09:27 +0800

    u

--- browser/d3/tree/index.js
+++ browser/d3/tree/index.js
@@ -1,382 +1,278 @@
-(function() {
+(function () {
     let treeData = {
-        "name": "XXX总部",
+        "name": "XXX总",
         "children": [{
-            "name": "a事业部",
+            "name": "a事业",
             "children": [{
-                "name": "IAAS研发中心",
+                "name": "IAA",
                 "children": null,
                 "orgCode": "2901"
             }, {
-                "name": "中间件研发中心",
+                "name": "中间",
                 "children": null,
                 "orgCode": "2902"
             }, {
-                "name": "云商城研发中心",
+                "name": "云商",
                 "children": null,
                 "orgCode": "2903"
             }, {
-                "name": "内核研发中心",
+                "name": "内核",
                 "children": null,
                 "orgCode": "2904"
             }, {
-                "name": "大数据平台研发中心",
+                "name": "大数",
                 "children": null,
                 "orgCode": "2905"
             }, {
-                "name": "安全研发中心",
+                "name": "安全",
                 "children": null,
                 "orgCode": "2906"
             }, {
-                "name": "数据库研发中心",
+                "name": "据库",
                 "children": null,
                 "orgCode": "2907"
             }, {
-                "name": "监控研发中心",
+                "name": "监控",
                 "children": null,
                 "orgCode": "2908"
             }, {
-                "name": "自开发SAAS研发中心",
+                "name": "自开",
                 "children": null,
                 "orgCode": "2909"
             }],
             "orgCode": "1319"
         }, {
-            "name": "员工平台研发中心",
+            "name": "员工",
             "children": [{
-                "name": "人事办公产品研发中心",
+                "name": "事办",
                 "children": null,
                 "orgCode": "2937"
             }, {
-                "name": "共享产品研发中心",
+                "name": "共享",
                 "children": null,
                 "orgCode": "2938"
             }, {
-                "name": "员工平台产品中心",
+                "name": "员工",
                 "children": null,
                 "orgCode": "2939"
             }, {
-                "name": "员工门户研发中心",
+                "name": "工门",
                 "children": null,
                 "orgCode": "2940"
             }, {
-                "name": "地产研发中心",
+                "name": "地产",
                 "children": null,
                 "orgCode": "2941"
             }, {
-                "name": "总经办",
+                "name": "经办",
                 "children": null,
                 "orgCode": "2942"
             }, {
-                "name": "豆芽产品研发中心",
+                "name": "bbb",
                 "children": null,
                 "orgCode": "2943"
             }, {
-                "name": "财务支持产品研发中心0",
+                "name": "务支",
                 "children": null,
                 "orgCode": "2944"
             }, {
-                "name": "财务核算产品研发中心0",
+                "name": "务核",
                 "children": null,
                 "orgCode": "2945"
             }],
             "orgCode": "1320"
         }, {
-            "name": "消费者平台研发中心",
+            "name": "c",
             "children": [{
-                "name": "BI部门",
-                "children": null,
-                "orgCode": "2990"
-            }, {
-                "name": "会员产品研发中心",
+                "name": "会员",
                 "children": null,
                 "orgCode": "2991"
             }, {
-                "name": "基础产品研发中心",
+                "name": "基础",
                 "children": null,
                 "orgCode": "2992"
             }, {
-                "name": "广告研发中心",
+                "name": "广告",
                 "children": null,
                 "orgCode": "2993"
             }, {
-                "name": "性能测试研发部",
+                "name": "能测",
                 "children": null,
                 "orgCode": "2994"
             }, {
-                "name": "总经办",
+                "name": "经办",
                 "children": null,
                 "orgCode": "2995"
             }, {
-                "name": "搜索研发中心",
+                "name": "搜索",
                 "children": null,
                 "orgCode": "2996"
             }, {
-                "name": "服务端架构部",
+                "name": "务端",
                 "children": null,
                 "orgCode": "2997"
             }, {
-                "name": "核心交易研发中心",
+                "name": "心交",
                 "children": null,
                 "orgCode": "2998"
             }, {
-                "name": "移动研发中心",
+                "name": "移动",
                 "children": null,
                 "orgCode": "2999"
             }, {
-                "name": "营销产品研发中心",
+                "name": "营销",
                 "children": null,
                 "orgCode": "3000"
             }, {
-                "name": "门店研发中心",
+                "name": "门店",
                 "children": null,
                 "orgCode": "3001"
             }, {
-                "name": "项目管理部",
+                "name": "项目",
                 "children": null,
                 "orgCode": "3002"
             }, {
-                "name": "频道及类目产品研发中心",
+                "name": "道及",
                 "children": null,
                 "orgCode": "3003"
             }],
             "orgCode": "1321"
         }, {
-            "name": "b研发中心",
+            "name": "b",
             "children": [{
-                "name": "产品部",
-                "children": null,
-                "orgCode": "2927"
-            }, {
-                "name": "平台开发部",
+                "name": "开发",
                 "children": null,
                 "orgCode": "2928"
             }, {
-                "name": "总监办",
+                "name": "监办",
                 "children": null,
                 "orgCode": "2929"
             }],
             "orgCode": "1322"
         }, {
-            "name": "技术支撑研发中心",
+            "name": "技术支撑",
             "children": [{
-                "name": "云计算研发中心",
+                "name": "云计",
                 "children": null,
                 "orgCode": "2957"
             }, {
-                "name": "大数据中心",
+                "name": "大数",
                 "children": null,
                 "orgCode": "2958"
             }, {
-                "name": "运营商研发中心",
+                "name": "营商",
                 "children": null,
                 "orgCode": "2959"
             }],
             "orgCode": "1323"
         }, {
-            "name": "零售云研发中心",
+            "name": "零售云",
             "children": [{
-                "name": "分销研发中心",
+                "name": "分销",
                 "children": null,
                 "orgCode": "3051"
             }, {
-                "name": "前端开发部",
+                "name": "端开",
                 "children": null,
                 "orgCode": "3052"
             }, {
-                "name": "大数据研发部",
+                "name": "据研",
                 "children": null,
                 "orgCode": "3053"
             }, {
-                "name": "客服研发中心",
+                "name": "客服",
                 "children": null,
                 "orgCode": "3054"
             }, {
-                "name": "苏宁小店研发中心",
+                "name": "宁小",
                 "children": null,
                 "orgCode": "3055"
             }],
             "orgCode": "1324"
         }, {
-            "name": "数据云公司",
+            "name": "ccc",
             "children": [{
-                "name": "CDN及存储研发中心",
-                "children": null,
-                "orgCode": "2968"
-            }, {
-                "name": "IaaS研发中心",
-                "children": null,
-                "orgCode": "2969"
-            }, {
-                "name": "中间件研发中心",
-                "children": null,
-                "orgCode": "2970"
-            }, {
-                "name": "云商城研发中心",
-                "children": null,
-                "orgCode": "2971"
-            }, {
-                "name": "体验设计部",
-                "children": null,
-                "orgCode": "2972"
-            }, {
-                "name": "公共平台及组件研发中心",
-                "children": null,
-                "orgCode": "2973"
-            }, {
-                "name": "前端开发部",
-                "children": null,
-                "orgCode": "2974"
-            }, {
-                "name": "商用产品研发中心",
-                "children": null,
-                "orgCode": "2975"
-            }, {
-                "name": "大数据平台研发中心",
-                "children": null,
-                "orgCode": "2976"
-            }, {
-                "name": "安全研发中心",
-                "children": null,
-                "orgCode": "2977"
-            }, {
-                "name": "开发工具研发中心",
-                "children": null,
-                "orgCode": "2978"
-            }, {
-                "name": "数据库研发中心",
-                "children": null,
-                "orgCode": "2979"
-            }, {
-                "name": "测试平台研发中心",
-                "children": null,
-                "orgCode": "2980"
-            }, {
-                "name": "监控研发中心",
-                "children": null,
-                "orgCode": "2981"
-            }, {
-                "name": "运营中心",
-                "children": null,
-                "orgCode": "2982"
-            }],
-            "orgCode": "1325"
-        }, {
-            "name": "c研发中心",
-            "children": [{
-                "name": "上海研发中心",
+                "name": "上海",
                 "children": null,
                 "orgCode": "3026"
             }, {
-                "name": "保险众筹及储值卡研发中心",
+                "name": "众筹",
                 "children": null,
                 "orgCode": "3027"
             }, {
-                "name": "保险及储值卡产品研发中心",
+                "name": "保险",
                 "children": null,
                 "orgCode": "3028"
             }, {
-                "name": "小微贷款产品研发中心",
+                "name": "小微",
                 "children": null,
                 "orgCode": "3029"
             }, {
-                "name": "总经办",
+                "name": "总经",
                 "children": null,
                 "orgCode": "3030"
             }, {
-                "name": "技术管理中心",
+                "name": "技术",
                 "children": null,
                 "orgCode": "3031"
             }, {
-                "name": "支付平台产品研发中心",
+                "name": "支付",
                 "children": null,
                 "orgCode": "3032"
             }, {
-                "name": "智能风控产品研发中心",
+                "name": "风控",
                 "children": null,
                 "orgCode": "3033"
             }, {
-                "name": "测试管理中心",
+                "name": "测试",
                 "children": null,
                 "orgCode": "3034"
             }, {
-                "name": "清结算产品研发中心",
+                "name": "清结",
                 "children": null,
                 "orgCode": "3035"
             }, {
-                "name": "理财产品研发中心",
+                "name": "理财",
                 "children": null,
                 "orgCode": "3036"
             }, {
-                "name": "移动平台及支付场景研发中心",
+                "name": "场景",
                 "children": null,
                 "orgCode": "3037"
             }, {
-                "name": "移动平台支付场景研发中心",
+                "name": "移动",
                 "children": null,
                 "orgCode": "3038"
             }, {
-                "name": "金融会员营销产品研发中心",
+                "name": "会员",
                 "children": null,
                 "orgCode": "3039"
             }, {
-                "name": "金融账务核心产品研发中心",
+                "name": "核心",
                 "children": null,
                 "orgCode": "3040"
             }],
             "orgCode": "1327"
         }, {
-            "name": "后台服务研发中心",
+            "name": "后台服务",
             "children": [{
-                "name": "售后产品中心",
+                "name": "售后",
                 "children": null,
                 "orgCode": "2932"
             }, {
-                "name": "物流平台产品中心",
+                "name": "物流",
                 "children": null,
                 "orgCode": "2933"
             }, {
-                "name": "物流研发中心",
+                "name": "物流",
                 "children": null,
                 "orgCode": "2934"
             }, {
-                "name": "运配运营支撑产品中心",
+                "name": "运配",
                 "children": null,
                 "orgCode": "2935"
             }],
             "orgCode": "1328"
-        }, {
-            "name": "数据中心",
-            "children": [{
-                "name": "IT基础服务中心",
-                "children": null,
-                "orgCode": "2961"
-            }, {
-                "name": "中间件管理中心",
-                "children": null,
-                "orgCode": "2962"
-            }, {
-                "name": "基础设施管理中心",
-                "children": null,
-                "orgCode": "2963"
-            }, {
-                "name": "安全管理部",
-                "children": null,
-                "orgCode": "2964"
-            }, {
-                "name": "运维中心",
-                "children": null,
-                "orgCode": "2965"
-            }, {
-                "name": "运维平台研发中心",
-                "children": null,
-                "orgCode": "2966"
-            }, {
-                "name": "金服管理中心",
-                "children": null,
-                "orgCode": "2967"
-            }],
-            "orgCode": "1329"
         }],
         "orgCode": "1000"
     }
@@ -416,18 +312,18 @@
         var nodes = treeData.descendants(),
             links = treeData.descendants().slice(1);
 
-        nodes.forEach(function(d) {
+        nodes.forEach(function (d) {
             d.y = d.depth * 180
         });
 
         var node = g.selectAll('g.node')
-            .data(nodes, function(d) {
+            .data(nodes, function (d) {
                 return d.id || (d.id = ++i);
             });
 
         var nodeEnter = node.enter().append('g')
             .attr('class', 'node')
-            .attr("transform", function(d) {
+            .attr("transform", function (d) {
                 return "translate(" + source.y0 + "," + source.x0 + ")";
             })
             .on('click', click);
@@ -435,19 +331,19 @@
         nodeEnter.append('circle')
             .attr('class', 'node')
             .attr('r', 1e-6)
-            .style("fill", function(d) {
+            .style("fill", function (d) {
                 return d._children ? "lightsteelblue" : "#fff";
             });
 
         nodeEnter.append('text')
             .attr("dy", ".35em")
-            .attr("x", function(d) {
+            .attr("x", function (d) {
                 return d.children || d._children ? -13 : 13;
             })
-            .attr("text-anchor", function(d) {
+            .attr("text-anchor", function (d) {
                 return d.children || d._children ? "end" : "start";
             })
-            .text(function(d) {
+            .text(function (d) {
                 return d.data.name;
             });
 
@@ -455,13 +351,13 @@
 
         nodeUpdate.transition()
             .duration(duration)
-            .attr("transform", function(d) {
+            .attr("transform", function (d) {
                 return "translate(" + d.y + "," + d.x + ")";
             });
 
         nodeUpdate.select('circle.node')
             .attr('r', 10)
-            .style("fill", function(d) {
+            .style("fill", function (d) {
                 return d._children ? "lightsteelblue" : "#fff";
             })
             .attr('cursor', 'pointer');
@@ -469,7 +365,7 @@
 
         var nodeExit = node.exit().transition()
             .duration(duration)
-            .attr("transform", function(d) {
+            .attr("transform", function (d) {
                 return "translate(" + source.y + "," + source.x + ")";
             })
             .remove();
@@ -481,13 +377,13 @@
             .style('fill-opacity', 1e-6);
 
         var link = g.selectAll('path.link')
-            .data(links, function(d) {
+            .data(links, function (d) {
                 return d.id;
             });
 
         var linkEnter = link.enter().insert('path', "g")
             .attr("class", "link")
-            .attr('d', function(d) {
+            .attr('d', function (d) {
                 var o = {
                     x: source.x0,
                     y: source.y0
@@ -499,13 +395,13 @@
 
         linkUpdate.transition()
             .duration(duration)
-            .attr('d', function(d) {
+            .attr('d', function (d) {
                 return diagonal(d, d.parent)
             });
 
         var linkExit = link.exit().transition()
             .duration(duration)
-            .attr('d', function(d) {
+            .attr('d', function (d) {
                 var o = {
                     x: source.x,
                     y: source.y
@@ -514,7 +410,7 @@
             })
             .remove();
 
-        nodes.forEach(function(d) {
+        nodes.forEach(function (d) {
             d.x0 = d.x;
             d.y0 = d.y;
         });
@@ -528,9 +424,9 @@
 
             return path
         }
-        
+
         let num = 0
-        
+
         function click(d) {
             if (d.children) {
                 //收起
@@ -577,7 +473,7 @@
             if (height > svg.attr('height')) {
                 svg.attr('height', height)
             } else
-                setTimeout(function() {
+                setTimeout(function () {
                     svg.attr('height', height)
                 }, 1000);
             treemap = d3.tree().size([height, width])
